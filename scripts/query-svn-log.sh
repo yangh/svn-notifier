6a2bff88fa27f8b7089c238b26f1f3287bd8ff4e
Yang Hong <yanghong@thunderst.com>
2010-04-15 16:05:44 +0800

    Added a script to do the same thing what I want.
    It's another sign of that the script language can do thing well.:)

---
+++ scripts/query-svn-log.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+usage()
+{
+    echo "Usage: $0 <path> <last_rev> <delay>"
+}
+
+if [ $# -lt 3 ]; then
+    usage
+    exit 1
+fi
+
+SVNPATH="$1"
+LASTREV="$2"
+DELAY="$3"
+
+while [ 1 -gt 0 ];
+do
+    #echo "Query svn log: -r$LASTREV:HEAD $SVNPATH"
+
+    svn log -r$LASTREV:HEAD "$SVNPATH" > .tmp 2>/dev/null
+
+    rn=`grep ^r[1-9] .tmp | tail -1 | awk '{print $1}'`
+    if [ "x$rn" != "x" ] ;then
+        n=${rn##r}
+        if [ $n -gt 0 ]; then
+            let LASTREV=n+1
+        fi
+    fi
+
+    cat .tmp
+    :> .tmp
+
+    sleep $DELAY
+done
+
